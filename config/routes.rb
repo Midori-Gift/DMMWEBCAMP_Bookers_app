ce2e0f484b371c542d0df54b643f0369db607710
Midori-Gift <okometaberoyo5009@gmail.com>
2021-05-08 13:40:53 +0000

    2021/05/08

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
   get 'top' => 'homes#top'
-  post 'books' => 'books#create'
-  post 'new' => 'books#new'
-  get 'edit' => 'books#edit'
-  get 'show' => 'books#show'
-  post 'show' => 'books#show'
 end
